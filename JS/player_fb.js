3b31fe59cd79a84aa21f21505395eda856c66ad6
Glitch (prairie-private-cowbell) <none>
2024-08-24 09:47:46 +0000

    💕👻 Checkpoint ./CSS/site_controller.css:39200795/2 ./host-27z.html:39200795/232 ./JS/player_fb.js:39200795/156

--- JS/player_fb.js
+++ JS/player_fb.js
@@ -221,8 +221,12 @@ $(function () {
 			}
 			
 			if(data.round == 1){
-        if((data.played_questions + 1) % 14 == 0) $('#info1').html('Thứ tự câu hỏi hiện tại: 14')
-				else $('#info1').html('Thứ tự câu hỏi hiện tại: ' + (data.played_questions + 1) % 14);
+        if((data.played_questions + 1) % 14 == 0) {
+          $('#gpx3').html('Thứ tự câu hỏi hiện tại: 14')
+        }
+				else {
+          $('#gpx3').html('Thứ tự câu hỏi hiện tại: ' + (data.played_questions + 1) % 14);
+        }
 			}
 			else if(data.round == 2 ){
 				$('#gpx3').html('Thứ tự câu hỏi hiện tại:<br/>' + (data.played_questions + 1) % 2);
